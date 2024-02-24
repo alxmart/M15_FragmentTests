da0ba8825b6d19142934c7698a0b946119794495
alxmart <alxmartinez@gmail.com>
2024-02-24 18:53:51 -0300

    Changes to use Fragment KTX

--- app/src/main/java/com/luizafmartinez/m15_fragmenttests/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m15_fragmenttests/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.luizafmartinez.m15_fragmenttests.fragments.ChamadasFragment
 import com.luizafmartinez.m15_fragmenttests.fragments.ConversasFragment
 
@@ -32,23 +34,33 @@ class MainActivity : AppCompatActivity() {
 
         btnMercado.setOnClickListener {
 
-            val conversasFragment = ConversasFragment()
+            //val conversasFragment = ConversasFragment()
 
             // passar dados de uma activity p/ outra, usa putExtra
             // passar dados para um fragment , usa arguments
 
-            val bundle = bundleOf(
+          /*  val bundle = bundleOf(
+                "categoria" to "mercado",
+                "usuario" to "Jamilton"
+            )*/
 
+            //conversasFragment.arguments = bundle
+
+           /* supportFragmentManager
+                .beginTransaction()
+                .replace( R.id.fragment_conteudo, conversasFragment )
+                .commit()*/
+
+            val bundle = bundleOf(
                 "categoria" to "mercado",
                 "usuario" to "Jamilton"
             )
 
-            conversasFragment.arguments = bundle
-
-            supportFragmentManager
-                .beginTransaction()
-                .replace( R.id.fragment_conteudo, conversasFragment )
-                .commit()
+            supportFragmentManager.commit {
+                replace<ConversasFragment>(
+                    R.id.fragment_conteudo,
+                    args = bundle)
+            }
         }
 
         btnChamadas.setOnClickListener {
